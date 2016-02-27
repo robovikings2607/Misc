1e095fb63d242860e827847d6c28af348eeabb79
FRC2607Dev <FRC2607Dev@gmail.com>
2016-02-27 15:58:01 -0500

    tested offline mp generation, working

--- MotionProfileGenerator/src/Main.java
+++ MotionProfileGenerator/src/Main.java
@@ -1,41 +1,49 @@
+import java.io.File;
+import java.io.FileWriter;
+
 import com.team254.lib.trajectory.Path;
 import com.team254.lib.trajectory.PathGenerator;
 import com.team254.lib.trajectory.Trajectory;
 import com.team254.lib.trajectory.TrajectoryGenerator;
 import com.team254.lib.trajectory.WaypointSequence;
 import com.team254.lib.trajectory.WaypointSequence.Waypoint;
+import com.team254.lib.trajectory.io.TextFileSerializer;
 
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Path generator");
 
-		SRXProfile test = new SRXProfile(-101.45, 0, -75, 200, 200, 10);
-		test.generateAndPushProfile(null);
+//		SRXProfile test = new SRXProfile(-101.45, 0, -75, 200, 200, 10);
+//		test.generateAndPushProfile(null);
 		
-/*		
-		TrajectoryGenerator.Config config = new TrajectoryGenerator.Config();
+    	TrajectoryGenerator.Config config = new TrajectoryGenerator.Config();
+        config.dt = .01;
+        config.max_acc = 8.0;
+        config.max_jerk = 60.0;
+        config.max_vel = 8.0;
+        
+        final double kWheelbaseWidth = 25.25/12;
 
-		config.dt = .01;			// i.e. 10ms
-		config.max_vel = 101.45;	// motor rotations / sec
-		config.max_acc = 500.0; 	// ?  (difference in velocity)
-		config.max_jerk = 1500.0;		// ?  (difference in acceleration)
+        WaypointSequence p = new WaypointSequence(10);
+        p.addWaypoint(new WaypointSequence.Waypoint(0, 0, 0));
+        //p.addWaypoint(new WaypointSequence.Waypoint(5, 7.0, 0));
+        p.addWaypoint(new WaypointSequence.Waypoint(50.0, 0.0, 0));
 
-		WaypointSequence p = new WaypointSequence(2);
-		p.addWaypoint(new Waypoint(0,0,0));
-		p.addWaypoint(new Waypoint(100,0,0));
-		
-		Path path = PathGenerator.makePath(p, config, 0, "ArmTest");
-		
-		Trajectory t = path.getLeftWheelTrajectory();
-		
-		System.out.println("Num segments: " + t.getNumSegments());
-		for (int i = 0; i < t.getNumSegments(); i++) {
-			Trajectory.Segment seg = t.getSegment(i);
-			System.out.println("Pos: " + seg.pos + " Vel: " + seg.vel + " Duration: " + seg.dt);
-		}
-*/	
-	
+        Path path = PathGenerator.makePath(p, config,
+            kWheelbaseWidth, "Corn Dogs");
+
+        TextFileSerializer tfs = new TextFileSerializer();
+        String traj = tfs.serialize(path);
+
+        try {
+        	FileWriter f = new FileWriter(new File("testTrajctory.txt"));
+        	f.write(traj);
+        	f.flush();
+        	f.close();
+        } catch (Exception e) {
+        	e.printStackTrace();
+        }
 	}
 
 }
