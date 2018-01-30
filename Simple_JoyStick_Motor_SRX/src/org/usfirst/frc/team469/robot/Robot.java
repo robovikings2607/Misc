245142cebde115afe13a5641947c3aa235ec81eb
Ramiro <ramiro.reinoso@verizon.net>
2018-01-30 01:24:56 -0500

    Moved initialization statements from teleoperiodic() to robotinit() where it is more appropriate.

--- Simple_JoyStick_Motor_SRX/src/org/usfirst/frc/team469/robot/Robot.java
+++ Simple_JoyStick_Motor_SRX/src/org/usfirst/frc/team469/robot/Robot.java
@@ -10,28 +10,30 @@ public class Robot extends IterativeRobot {
   
 	TalonSRX _talon = new TalonSRX(15);	
 	Joystick _joy = new Joystick(0);	
-
-	int _loops = 0;
+	
+	/* Percent voltage mode */
 	
 	int timeoutMs = 10;
 	
+	int _loops = 0;
+	
 	public void robotInit() {
- 
+		
+		_talon.configPeakOutputForward(0.5, timeoutMs);  // 50% peak voltage forward
+		_talon.configPeakOutputReverse(-0.5, timeoutMs);  // 50% peak voltage reverse
+		_talon.configNominalOutputForward(0.0, timeoutMs);
+		_talon.configNominalOutputReverse(0.0, timeoutMs);
+		_talon.setInverted(true);
 	}
+	
     /**
      * This function is called periodically during operator control
      */
     public void teleopPeriodic() {
     	/* get gamepad axis */
-    	double leftYstick = _joy.getRawAxis(1);
+      	double leftYstick = _joy.getRawAxis(1);
 
-        	/* Percent voltage mode */
-		_talon.configPeakOutputForward(0.5, timeoutMs);  // 50% peak voltage forward
-		_talon.configPeakOutputReverse(-0.5, timeoutMs);  // 50% peak voltage reverse
-		_talon.configNominalOutputForward(0.0, timeoutMs);
-		_talon.configNominalOutputReverse(0.0, timeoutMs);
-    		_talon.setInverted(true);
-        	_talon.set(ControlMode.PercentOutput, leftYstick);
+      	_talon.set(ControlMode.PercentOutput, leftYstick);
 
         if(++_loops >= 10) {
         	_loops = 0;
